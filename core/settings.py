1fd00a1f01bf4a98f9e30f24e243a7206be99dc3
Riaño John <jhon091199@gmail.com>
2024-04-17 20:53:33 -0500

    fix dockerfile

--- core/settings.py
+++ core/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+3m6m9bfs3*5^76wd=^m%5(89z8lpne8m$5gq+2gqep24^u__y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'microserves',
     'graphene_django'
-    
+
 ]
 
 MIDDLEWARE = [
