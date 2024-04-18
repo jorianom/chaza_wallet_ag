295ba98ff8633c8d1eb99836b4c94b4f864704d2
Riaño John <jhon091199@gmail.com>
2024-04-17 20:40:51 -0500

    fix dockerfile

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+3m6m9bfs3*5^76wd=^m%5(89z8lpne8m$5gq+2gqep24^u__y
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
