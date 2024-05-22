25fad37fc2f9975cd3747795dc1930c554a9957e
Riaño John <jhon091199@gmail.com>
2024-05-22 12:11:07 -0500

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 # ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
 ALLOWED_HOSTS = [
+    '35.238.88.129',
     'chaza-wallet-ag-yerq2evawq-uc.a.run.app',
     'chaza-wallet-ag-ithgocyoua-uc.a.run.app',
     '127.0.0.1',
