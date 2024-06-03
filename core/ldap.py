2c16f604f89c21ac693943b42e7faf949c0e0e52
Riaño John <jhon091199@gmail.com>
2024-06-02 20:49:53 -0500

    Update ldap.py

--- core/ldap.py
+++ core/ldap.py
@@ -2,7 +2,7 @@ from ldap3 import Server, Connection, ObjectDef, Reader, MODIFY_REPLACE, SIMPLE
 from graphql import GraphQLError
 
 # Conexión LDAP server
-ldap_server = Server('ldap://localhost:389')
+ldap_server = Server('ldap://10.59.243.219:389')
 ldap_user = 'cn=admin,dc=chazawallet,dc=unal,dc=edu,dc=co'
 ldap_password = 'admin'
 base_dn = 'dc=chazawallet,dc=unal,dc=edu,dc=co'
